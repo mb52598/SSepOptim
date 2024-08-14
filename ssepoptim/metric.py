Fixed cost_matrix calculation in pit wrapper

ssepoptim/metric.py
@@ -109,12 +109,12 @@ def scale_invariant_signal_to_distortion_ratio_pit(
     return fbss_pit_wrap(scale_invariant_signal_to_distortion_ratio, prediction, target)
 
 
-def signal_to_distortion_ratio_pit(
+def signal_to_distortion_ratio_fbss_pit(
     prediction: torch.Tensor,
     target: torch.Tensor,
 ) -> torch.Tensor:
     try:
         result = fbss_torch.sdr(target, prediction)
     except:
-        result = torch.inf
+        result = torch.tensor([torch.inf])
     return result

ssepoptim/metrics/pit.py
@@ -79,7 +79,10 @@ def fbss_pit_wrap(
     for pred, tgt in zip(prediction, target):
         pred = pred.unsqueeze(1).expand(-1, channels, -1)
         tgt = tgt.unsqueeze(0).expand(channels, -1, -1)
-        cost_mat = metric(prediction, target)
+        cost_mat = metric(pred, tgt)
+        assert cost_mat.dim() in [2, 3]
+        if cost_mat.dim() > 2:
+            cost_mat = torch.mean(cost_mat, dim=-1)
         rows, cols = linear_sum_assignment(cost_mat)
         values.append(torch.mean(cost_mat[rows, cols]))
     return torch.stack(values)